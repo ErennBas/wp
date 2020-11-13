30c547355abc5e06018cd00cb78d27ca5da76f66
ErennBas <62754751+ErennBas@users.noreply.github.com>
2020-11-13 20:47:06 +0300

    Add files via upload

---
+++ assets/js/all.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	$('.toggle-icon').click(function(){
+		$('.index-chat').toggleClass('show-icon');
+	});
+	
+	$('.toggle-search').click(function(){
+		$('.index-chat').toggleClass('show-search');
+	});
+	
+	$('.toggle-menu').click(function(){
+		$('.whatsapp-main').toggleClass('show-menu');
+	});
+	
+	
+	
+	
+}); 
